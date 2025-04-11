fa3d03176d521226242e4307778661448089aca6
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2025-04-10 23:53:15 -0500

    [UPD] payment_recurrente: Actualizacion API v2

--- payment_recurrente/models/payment_transaction.py
+++ payment_recurrente/models/payment_transaction.py
@@ -18,6 +18,7 @@ class PaymentTransaction(models.Model):
     id_recurrente_checkout = fields.Char(string="Recurrente Checkout ID")
     url_recurrente_checkout = fields.Char(string="Recurrente Checkout URL")
     product_recurrente_checkout = fields.Char(string="Recurrente Checkout Product")
+    payment_intent_failure_reasons = fields.Html(string="Payment Intent Failure Reasons", default="<ul></ul>", help="Here you can find the multiple reasons why the client did have payment problems.")
 
     def _get_specific_rendering_values(self, processing_values):
         """ Override of payment to return Recurrente-specific rendering values.
@@ -32,15 +33,36 @@ class PaymentTransaction(models.Model):
         if self.provider_code != 'recurrente':
             return res
 
+        token = self.provider_id.recurrente_setup_token
+        if not token:
+            setup_data = self.provider_id._recurrente_make_request("setup")
+            token = setup_data["token"]
+            self.provider_id.recurrente_setup_token = token
+
+        headers = {
+            "X-Token": token,
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
         # Initiate the payment and retrieve the payment link data.
-        number = int(self.reference.split("-")[0][2:])
+        correlative = self.reference.split("-")[0][2:]
+        number = int(correlative)
         name_split = self.partner_name.split(" ", maxsplit=1)
         name, surname = (name_split[0], name_split[1]) if len(name_split) > 1 else (name_split[0], "--")
+        
+        installments = ""
+        if self.provider_id.allow_installments:
+            installments = "|".join(inst.name for inst in self.provider_id.installment_ids)
+
         payload = {
             'number': number,
+            'correlative': correlative,
             'description': self.reference,
             'amount': self.amount,
             'currency': self.currency_id.name,
+            "allowTransfer": self.provider_id.allow_transfers,
+            "installments": installments,
             'redirection': {
                 "successUrl": f"{self.get_base_url()}{RecurrenteController._request_url}?tx_ref={self.reference}&status=request_success",
                 "cancelUrl": f"{self.get_base_url()}{RecurrenteController._request_url}?tx_ref={self.reference}&status=request_cancel",
@@ -54,11 +76,11 @@ class PaymentTransaction(models.Model):
                 "address": self.partner_address or "Ciudad",
             }
         }
-        payment_link_data = self.provider_id._recurrente_make_request('checkouts', payload=payload)
+        payment_link_data = self.provider_id._recurrente_make_request('checkouts/hosted/single', headers=headers, payload=payload)
 
         self.id_recurrente_checkout = payment_link_data["id"]
-        self.url_recurrente_checkout = payment_link_data["url"]
         self.product_recurrente_checkout = payment_link_data["product"]
+        self.url_recurrente_checkout = payment_link_data["url"]
 
         # Extract the payment link URL and embed it in the redirect form.
         rendering_values = {
@@ -108,6 +130,21 @@ class PaymentTransaction(models.Model):
         # tx._execute_callback()
         return tx
 
+    def _save_failure_reason(self, failure_reason):
+        timezone = self._context.get('tz') or self.env.user.tz or 'America/Guatemala'
+        self_tz = self.with_context(tz=timezone)
+        datetime = fields.Datetime.context_timestamp(self_tz, fields.Datetime.now())
+
+        failure_reason = f"<li>{datetime:%Y-%m-%d %H:%M:%S} {failure_reason}</li>"
+        actual_reasons = str(self.payment_intent_failure_reasons) or "<ul></ul>"
+
+        if "<ul>" in actual_reasons:
+            actual_reasons = actual_reasons.replace("<ul>", f"<ul>{failure_reason}")
+        else:
+            actual_reasons = f"<ul>{failure_reason}</ul>"
+        
+        self.payment_intent_failure_reasons = actual_reasons
+
     def _get_tx_from_webhook_data(self, notification_data):
         """ Find the transaction based on the webhook data.
 
@@ -152,8 +189,12 @@ class PaymentTransaction(models.Model):
 
         # Update the payment state.
         payment_status = notification_data['event_type'].lower()
+        failure_reason = notification_data.get('failure_reason', 'No reason.')
         if payment_status in const.PAYMENT_STATUS_MAPPING['pending']:
-            self._set_pending()
+            if self.state == "draft":
+                self._set_pending()
+            if failure_reason:
+                self._save_failure_reason(failure_reason)
         elif payment_status in const.PAYMENT_STATUS_MAPPING['done']:
             self._set_done()
         elif payment_status in const.PAYMENT_STATUS_MAPPING['error']:
